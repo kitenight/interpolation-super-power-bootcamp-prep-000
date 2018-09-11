faec6796cd2920561b3af003edbddd3f79a45ea8
Lyn <jen.l@mailfence.com>
2018-09-11 19:28:38 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,6 @@
 
 def display_rainbow (["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
   print "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]},"
+end
+
+display_rainbow()
