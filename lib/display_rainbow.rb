98228a845cfb644c8a87bc34cfe88cc41cdb0312
Lyn <jen.l@mailfence.com>
2018-09-11 19:52:29 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,5 +5,3 @@ colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow (colours)
   print "R: #{colours[0]}, O: #{colours[1]}, Y: #{colours[2]}, G: #{colours[3]}, B: #{colours[4]}, I: #{colours[5]}, V: #{colours[6]},"
 end
-
-display_rainbow
